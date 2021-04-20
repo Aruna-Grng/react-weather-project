7abd840c88159422ee1ed10723e3d5765e4e71a4
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-04-20 12:05:35 -0500

    Created IconDisplay component and its css and displayed icon in UI.

--- src/Components/CityDisplay.js
+++ src/Components/CityDisplay.js
@@ -1,24 +1,17 @@
 import React from "react";
 import "./CityDisplay.css";
-import ReactAnimatedWeather from "react-animated-weather";
 import HumidityInfo from "./HumidityInfo";
 import Temperature from "./Temperature";
 import WindSpeed from "./WindSpeed";
+import IconDisplay from "./IconDisplay";
 
 
 export default function CityDisplay(props) {
     return(
         <div>
-            <h1 className="city">Dallas</h1>
-            <h2>Sunny</h2>
-            <p>
-                <ReactAnimatedWeather
-                    icon={props.icon}
-                    color="#eeeeee"
-                    size={props.size}
-                    animate={true}
-                />
-            </p>
+            <h1 className="city">{props.data.city}</h1>
+            <h2 className="text-capitalize">{props.data.description}</h2>
+            <IconDisplay image={props.data.icon} />
             <div className="row justify-content-md-center">
                 <HumidityInfo />
                 <Temperature />
