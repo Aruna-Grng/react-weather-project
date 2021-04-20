6933d134325219ebf3275ddb322b29d17f09202b
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-04-20 11:07:24 -0500

    Moved CityDisplay component inside WeatherSearch component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import CityDisplay from './Components/CityDisplay';
+// import CityDisplay from './Components/CityDisplay';
 import WeatherSearch from "./Components/WeatherSearch";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <WeatherSearch />
-        <CityDisplay icon="CLEAR_DAY" size={200} />
+        <WeatherSearch defaultCity="Austin" />
+        {/* <CityDisplay icon="CLEAR_DAY" size={200} /> */}
       </div>
     </div>
   );
