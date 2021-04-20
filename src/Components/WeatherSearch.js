c78a8058806168acb3cff5259301eefa2c5719af
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-04-20 11:10:29 -0500

    Added functions in WeatherSearch component.

--- src/Components/WeatherSearch.js
+++ src/Components/WeatherSearch.js
@@ -1,43 +1,66 @@
 import axios from "axios";
-// import React, { useState } from "react";
+import React, { useState } from "react";
+import CityDisplay from "./CityDisplay";
 import "./WeatherSearch.css";
 
-export default function WeatherSearch() {
-    // const [city, setCity] = useState(props.city);
+export default function WeatherSearch(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    const [city, setCity] = useState(props.defaultCity);
 
-    // let city = "Dallas";
-    // let apiKey = "28a9b26783d5b53ed2f25d7dd7717889";
-    // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    // axios.get(apiUrl).then(search);
+    function handleSearch(response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            wind: response.data.wind.speed,
+            city: response.data.name,
+            description: response.data.weather[0].description,
+            icon: response.data.weather[0].icon,
+            humidity: response.data.main.humidity
+        });
+    }
 
-    // function search(response) {
-    //     console.log(response.data);
-    // }
-    
-    return (
-        <div>
-            <form>
-                <input 
-                className="city-name" 
-                style={{ backgroundColor: '#eeeeee' }} 
-                type="search" 
-                placeholder="Enter city name..."
-                autoFocus="on" /> {""}
+    function search() {
+        const apiKey = "28a9b26783d5b53ed2f25d7dd7717889";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleSearch);
+    }
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
 
-                <input 
-                className="search-btn" 
-                type="submit" 
-                value="Search" /> 
-            </form>
-        </div>
+    function handleCityChange(event) {
+        setCity(event.target.value);
+    }
+
+    if (weatherData.ready) {
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                    className="city-name" 
+                    style={{ backgroundColor: '#eeeeee' }} 
+                    type="search" 
+                    placeholder="Enter city name..."
+                    autoFocus="on"
+                    onChange={handleCityChange} /> {""}
+
+                    <input 
+                    className="search-btn" 
+                    type="submit" 
+                    value="Search" /> 
+                </form>
+                <CityDisplay data={weatherData} />
+{/* icon="CLEAR_DAY" size={200} */}
+            </div>
     );
+    } else {
+        search();
+
+        return "Loading...";
+    } 
 }
 
 
-    // function handleSearch(event) {
-    //     event.preventDefault();
-        
-
-    // function handleCityUpdate(event) {
-    //     setCity(event.target.value);
-    // }
+    
