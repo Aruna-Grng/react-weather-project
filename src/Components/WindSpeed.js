aa7386f36abb91497f6d2ac62e6d205659678ad1
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-04-20 23:32:29 -0500

    Added math.round in windspeed.

--- src/Components/WindSpeed.js
+++ src/Components/WindSpeed.js
@@ -4,7 +4,7 @@ export default function WindSpeed(props) {
     return (
         <div className="col-md-2 text-center">
             <span style={{ fontWeight:"bold", opacity: .6 }}>Wind</span>{" "}
-            <span style={{ fontWeight: 600, fontSize: 14, opacity: .6 }}>{props.wind}km/h</span>
+            <span style={{ fontWeight: 600, fontSize: 14, opacity: .6 }}>{Math.round(props.wind)}km/h</span>
         </div>
     )
 }
