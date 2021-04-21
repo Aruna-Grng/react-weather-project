968b890f3bf33df800475e5b3f19661b27cb9c5f
Aruna-Grng <68351192+Aruna-Grng@users.noreply.github.com>
2021-04-20 22:12:26 -0500

    Added marginTop in temperature.

--- src/Components/Temperature.js
+++ src/Components/Temperature.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Temperature(props) {
     return (
-        <div className="col-md-2 text-center">
+        <div className="col-md-2 text-center" style={{ marginTop: -11 }}>
             <span style={{ fontSize: 43, opacity: .7, fontWeight: 650 }}>{Math.round(props.displayTemperature)}°</span>
-            <span style={{ position: "relative", top: -16, opacity: .7, fontWeight: 650 }}>C</span>
+            <span style={{ position: "relative", top: -18, opacity: .7, fontWeight: 650 }}>C</span>
         </div>
     )
 }
